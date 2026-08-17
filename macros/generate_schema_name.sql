12193fe22533eaa1bacee24cec10c257a2b020c6
Priyansh Mathur <priyansh.mathur@accordion.com>
2026-08-17 04:02:04 +0000

    generate_schema_name macro updated

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,17 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+{% macro generate_schema_name(custom_schema_name, node) %}
 
-    {% set default_schema = target.schema %}
+    {% if target.name == 'prod' %}
 
-    {% if custom_schema_name is none %}
-        {{ default_schema }}
+        {{ custom_schema_name | trim if custom_schema_name else target.schema }}
+
     {% else %}
-        {{ custom_schema_name | trim }}
+
+        {% if custom_schema_name %}
+            {{ target.schema }}_{{ custom_schema_name | trim }}
+        {% else %}
+            {{ target.schema }}
+        {% endif %}
+
     {% endif %}
 
-{%- endmacro %}
+{% endmacro %}
